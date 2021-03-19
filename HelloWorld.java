5634ef378a7af7c4d65b84eab3fee1ac47c4a0d2
danilo-dellorco <danilodellorcolp@gmail.com>
2021-03-19 18:41:00 +0100

    as
    wq:
    qw:
    
    sa
    :wq
    :wq

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main() {
+		int a = 5;
+		if (a == 6) {
+			System.out.println("Hello, World !");
+		}
+		else{
+			System.out.println("Goodbye, World ...");
+		}
+	}
+}
